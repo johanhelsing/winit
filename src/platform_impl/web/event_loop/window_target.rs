e5e3abbadeea5e9700bc9a0f00a53192776f798f
Dany Sluijk <me@dany.dev>
2021-05-20 11:11:21 +0200

    feat: add pointer events to web
    feat: remove PointerType for touch events
    
    Remove duplicate
    
    Changelog and features
    
    Remove PointerType
    
    feat: renamed events, added touch type guard
    
    Rename
    
    Flip the y axis
    
    Fix physical position and add force
    
    Update comment
    
    Update features
    
    Use normalized force
    
    Remove unnecessary todos
    
    Update comment
    
    Refactor add touch_handler
    
    Rephrase by Liamolucko

--- src/platform_impl/web/event_loop/window_target.rs
+++ src/platform_impl/web/event_loop/window_target.rs
@@ -1,7 +1,7 @@
 use super::{super::monitor, backend, device, proxy::Proxy, runner, window};
 use crate::dpi::{PhysicalSize, Size};
 use crate::event::{
-    DeviceEvent, DeviceId, ElementState, Event, KeyboardInput, TouchPhase, WindowEvent,
+    DeviceEvent, DeviceId, ElementState, Event, KeyboardInput, Touch, TouchPhase, WindowEvent,
 };
 use crate::event_loop::ControlFlow;
 use crate::monitor::MonitorHandle as RootMH;
@@ -229,6 +229,62 @@ impl<T> WindowTarget<T> {
                 event: WindowEvent::Resized(new_size),
             });
             runner.request_redraw(WindowId(id));
+        });
+
+        let runner = self.runner.clone();
+        canvas.on_touch_move(move |device_id, location, force| {
+            runner.send_event(Event::WindowEvent {
+                window_id: RootWindowId(id),
+                event: WindowEvent::Touch(Touch {
+                    id: device_id as u64,
+                    device_id: RootDeviceId(DeviceId(device_id)),
+                    phase: TouchPhase::Moved,
+                    force: Some(force),
+                    location,
+                }),
+            });
+        });
+
+        let runner = self.runner.clone();
+        canvas.on_touch_down(move |device_id, location, force| {
+            runner.send_event(Event::WindowEvent {
+                window_id: RootWindowId(id),
+                event: WindowEvent::Touch(Touch {
+                    id: device_id as u64,
+                    device_id: RootDeviceId(DeviceId(device_id)),
+                    phase: TouchPhase::Started,
+                    force: Some(force),
+                    location,
+                }),
+            });
+        });
+
+        let runner = self.runner.clone();
+        canvas.on_touch_up(move |device_id, location, force| {
+            runner.send_event(Event::WindowEvent {
+                window_id: RootWindowId(id),
+                event: WindowEvent::Touch(Touch {
+                    id: device_id as u64,
+                    device_id: RootDeviceId(DeviceId(device_id)),
+                    phase: TouchPhase::Ended,
+                    force: Some(force),
+                    location,
+                }),
+            });
+        });
+
+        let runner = self.runner.clone();
+        canvas.on_touch_cancel(move |device_id, location, force| {
+            runner.send_event(Event::WindowEvent {
+                window_id: RootWindowId(id),
+                event: WindowEvent::Touch(Touch {
+                    id: device_id as u64,
+                    device_id: RootDeviceId(DeviceId(device_id)),
+                    phase: TouchPhase::Cancelled,
+                    force: Some(force),
+                    location,
+                }),
+            });
         });
 
         let runner = self.runner.clone();
--- src/platform_impl/web/web_sys/canvas/pointer_handler.rs
+++ src/platform_impl/web/web_sys/canvas/pointer_handler.rs
@@ -1,8 +1,10 @@
 use super::event;
 use super::EventListenerHandle;
 use crate::dpi::PhysicalPosition;
+use crate::event::Force;
 use crate::event::{ModifiersState, MouseButton};
 
+use web_sys::HtmlCanvasElement;
 use web_sys::PointerEvent;
 
 #[allow(dead_code)]
@@ -12,6 +14,10 @@ pub(super) struct PointerHandler {
     on_cursor_move: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
     on_pointer_press: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
     on_pointer_release: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
+    on_touch_move: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
+    on_touch_down: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
+    on_touch_up: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
+    on_touch_cancel: Option<EventListenerHandle<dyn FnMut(PointerEvent)>>,
 }
 
 impl PointerHandler {
@@ -22,6 +28,10 @@ impl PointerHandler {
             on_cursor_move: None,
             on_pointer_press: None,
             on_pointer_release: None,
+            on_touch_move: None,
+            on_touch_down: None,
+            on_touch_up: None,
+            on_touch_cancel: None,
         }
     }
 
@@ -105,11 +115,79 @@ impl PointerHandler {
         ));
     }
 
+    pub fn on_touch_move<F>(&mut self, canvas_common: &super::Common, handler: F)
+    where
+        F: 'static + FnMut(i32, PhysicalPosition<f64>, Force),
+    {
+        self.on_touch_move =
+            Some(canvas_common.add_event("pointermove", touch_handler(handler, canvas_common)));
+    }
+
+    pub fn on_touch_down<F>(&mut self, canvas_common: &super::Common, handler: F)
+    where
+        F: 'static + FnMut(i32, PhysicalPosition<f64>, Force),
+    {
+        self.on_touch_down =
+            Some(canvas_common.add_event("pointerdown", touch_handler(handler, canvas_common)));
+    }
+
+    pub fn on_touch_up<F>(&mut self, canvas_common: &super::Common, handler: F)
+    where
+        F: 'static + FnMut(i32, PhysicalPosition<f64>, Force),
+    {
+        self.on_touch_up =
+            Some(canvas_common.add_event("pointerup", touch_handler(handler, canvas_common)));
+    }
+
+    pub fn on_touch_cancel<F>(&mut self, canvas_common: &super::Common, handler: F)
+    where
+        F: 'static + FnMut(i32, PhysicalPosition<f64>, Force),
+    {
+        self.on_touch_cancel =
+            Some(canvas_common.add_event("pointercancel", touch_handler(handler, canvas_common)));
+    }
+
     pub fn remove_listeners(&mut self) {
         self.on_cursor_leave = None;
         self.on_cursor_enter = None;
         self.on_cursor_move = None;
         self.on_pointer_press = None;
         self.on_pointer_release = None;
+        self.on_touch_move = None;
+        self.on_touch_down = None;
+        self.on_touch_up = None;
+        self.on_touch_cancel = None;
+    }
+}
+
+fn touch_handler<F>(mut handler: F, canvas_common: &super::Common) -> impl FnMut(PointerEvent)
+where
+    F: 'static + FnMut(i32, PhysicalPosition<f64>, Force),
+{
+    let canvas = canvas_common.raw.clone();
+    move |event: PointerEvent| {
+        if event.pointer_type() != "touch" {
+            return;
+        }
+
+        handler(
+            event.pointer_id(),
+            touch_physical_position(&event, &canvas),
+            Force::Normalized(event.pressure() as f64),
+        );
+    }
+}
+
+fn touch_physical_position(
+    event: &PointerEvent,
+    canvas: &HtmlCanvasElement,
+) -> PhysicalPosition<f64> {
+    // Calculate the scale factor every time, since it's possible for the canvas's width and height attributes to get out of sync with its actual HTML size.
+    let dpi_width = canvas.width() as f64 / canvas.offset_width() as f64;
+    let dpi_height = canvas.height() as f64 / canvas.offset_height() as f64;
+    PhysicalPosition {
+        x: event.client_x() as f64 * dpi_width,
+        // Flip the Y axis because canvas's origin is top-left.
+        y: canvas.height() as f64 - event.client_y() as f64 * dpi_height,
     }
 }
